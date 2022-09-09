c0f160000ffaae84a11ce8a5abc53fbfcc3987e9
JAMES EMEDIONG <jemediong2001@gmail.com>
2022-09-09 01:41:43 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+/**
+* Descrition main - always entry.
+*
+* Return: 0 if success.
+*
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	Return (0);
+
+}
