f8b84855d92d41ec608762b028e7e4e02db89f99
JAMES EMEDIONG <jemediong2001@gmail.com>
2022-09-09 13:26:15 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,12 @@
 int main(void)
 {
 int d;
-for (d = 0; d < 90; d++)
+for (d = 0; d < 100; d++)
 {
 putchar((d / 10) + '0');
 putchar((d % 10) + '0');
 
-if (d != 89)
+if (d != 99)
 {
 putchar(',');
 putchar(' ');
