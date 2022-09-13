70fb902e72bec6c52f509a294b2be071cf60aaab
JAMES EMEDIONG <jemediong2001@gmail.com>
2022-09-13 06:03:52 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * main - prints _putchar
+ *
+ * Description: print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
+}
+
