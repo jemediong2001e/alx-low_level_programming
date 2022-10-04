5a031d628866fe17b9c1a31862732da5431d92e9
JAMES EMEDIONG <jemediong2001@gmail.com>
2022-10-04 02:45:18 -0700

    maybe

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,6 +5,7 @@
  * str_concat - concatenates two strings.
  * @s1: first string.
  * @s2: second string.
+ *
  * Return: pointer of an array of chars
  */
 
@@ -15,6 +16,8 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
