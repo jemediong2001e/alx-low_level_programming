8c805d791c1e30aea628e2344473f9a623f4533c
JAMES EMEDIONG <jemediong2001@gmail.com>
2022-10-04 03:06:14 -0700

    maybe

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -5,6 +5,7 @@
  * ch_free_grid - frees a 2 dimensional array.
  * @grid: multidimensional array of char.
  * @height: height of the array.
+ *
  * Return: no return
  */
 
@@ -22,6 +23,7 @@ void ch_free_grid(char **grid, unsigned int height)
 /**
  * strtow - splits a string into words.
  * @str: string.
+ *
  * Return: pointer of an array of integers
  */
 
